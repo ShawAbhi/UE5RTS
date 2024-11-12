7c5ad29327a1a00b41d40400ab60ae5dc984569c
Shaw <abhishek.shaw@gameskraft.com>
2024-11-12 20:40:29 +0530

    Added hold to move for props and revamped touch system

---
+++ Source/UE5RTS/Pawn/ControlPlayer.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ControlPlayer.generated.h"
+
+UCLASS()
+class UE5RTS_API AControlPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AControlPlayer();
+
+};
